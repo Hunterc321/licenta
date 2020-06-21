bd319edb7fb16019e1bd2fe291e3b150a60d10b1
Popa Cosmin <popacosmin50@yahoo.com>
2020-06-22 00:08:24 +0300

    Adds changes

---
+++ flutter_app/lib/conference_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ConferenceList extends StatefulWidget {
+  @override
+  _ConferenceListState createState() => _ConferenceListState();
+}
+
+class _ConferenceListState extends State<ConferenceList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- flutter_app/lib/login_screen.dart
+++ flutter_app/lib/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutterapp/conference_list.dart';
 
 class LoginScreen extends StatelessWidget {
   final Color backgroundColor1;
@@ -22,131 +23,136 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
       height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(top: 10.0, bottom: 50.0),
-            child: Center(
-              child: new Column(
-                children: <Widget>[
-                  Container(
-                    height: 300.0,
-                    width: 300.0,
-                    child: this.logo,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 50.0),
+              child: Center(
+                child: new Column(
+                  children: <Widget>[
+                    Container(
+                      height: 300.0,
+                      width: 300.0,
+                      child: this.logo,
 
-                  ),
-                  new Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: new Text(
-                      "Timi Lupascu",
-                      style: TextStyle(color: this.foregroundColor),
                     ),
-                  )
+                    new Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: new Text(
+                        "Timi Lupascu",
+                        style: TextStyle(color: this.foregroundColor),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      color: this.foregroundColor,
+                      width: 0.5,
+                      style: BorderStyle.solid),
+                ),
+              ),
+              padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+              child: new Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  new Padding(
+                    padding:
+                    EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
+                    child: Icon(
+                      Icons.alternate_email,
+                      color: this.foregroundColor,
+                    ),
+                  ),
+                  new Expanded(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Tudoran@live.com',
+                        hintStyle: TextStyle(color: this.foregroundColor),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-          ),
-          new Container(
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.only(left: 40.0, right: 40.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                    color: this.foregroundColor,
-                    width: 0.5,
-                    style: BorderStyle.solid),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      color: this.foregroundColor,
+                      width: 0.5,
+                      style: BorderStyle.solid),
+                ),
+              ),
+              padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+              child: new Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  new Padding(
+                    padding:
+                    EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
+                    child: Icon(
+                      Icons.lock_open,
+                      color: this.foregroundColor,
+                    ),
+                  ),
+                  new Expanded(
+                    child: TextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'pass',
+                        hintStyle: TextStyle(color: this.foregroundColor),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            padding: const EdgeInsets.only(left: 0.0, right: 10.0),
-            child: new Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                new Padding(
-                  padding:
-                  EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
-                  child: Icon(
-                    Icons.alternate_email,
-                    color: this.foregroundColor,
-                  ),
-                ),
-                new Expanded(
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Tudoran@live.com',
-                      hintStyle: TextStyle(color: this.foregroundColor),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
+              alignment: Alignment.center,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new FlatButton(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 20.0),
+                      color: this.highlightColor,
+                      onPressed: () => { Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ConferenceList()),
+                      )},
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(color: this.foregroundColor),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          new Container(
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                    color: this.foregroundColor,
-                    width: 0.5,
-                    style: BorderStyle.solid),
+                ],
               ),
             ),
-            padding: const EdgeInsets.only(left: 0.0, right: 10.0),
-            child: new Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                new Padding(
-                  padding:
-                  EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
-                  child: Icon(
-                    Icons.lock_open,
-                    color: this.foregroundColor,
-                  ),
-                ),
-                new Expanded(
-                  child: TextField(
-                    obscureText: true,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'pass',
-                      hintStyle: TextStyle(color: this.foregroundColor),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          new Container(
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
-            alignment: Alignment.center,
-            child: new Row(
-              children: <Widget>[
-                new Expanded(
-                  child: new FlatButton(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20.0, horizontal: 20.0),
-                    color: this.highlightColor,
-                    onPressed: () => {},
-                    child: Text(
-                      "Log In",
-                      style: TextStyle(color: this.foregroundColor),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
 
 
-        ],
+          ],
+        ),
       ),
     );
   }
